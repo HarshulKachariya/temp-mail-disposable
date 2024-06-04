b04c6c69d797436b780a04bd6e511b24066826c0
harshil sakhiya <95619537+harshilsakhiya@users.noreply.github.com>
2024-06-04 14:13:21 +0530

    feat : add google key add

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="google-site-verification" content="WuFxV91DgUW_CNbRf45kqrU3eFpS49q5qqTBnGLhpCM" />
+      </Head>
       <body>
         <Main />
         <NextScript />
