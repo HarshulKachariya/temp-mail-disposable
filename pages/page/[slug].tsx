de8243870cc3b152155c275e378fd8ea3e62aa36
HarshulKachariya <harshulkachariya17@gmail.com>
2024-06-06 00:36:06 +0530

    "Removed react-icons dependency and updated page/[slug].tsx to import CopyrightPage instead of Footer"

--- pages/page/[slug].tsx
+++ pages/page/[slug].tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import Header from "../../components/header";
+
 import HomePage from "@/components/home/home";
 import Blogs from "@/pages/blogs";
-import Footer from "@/components/copyrightPage/index";
+
+import CopyrightPage from "@/components/copyrightPage/index";
 
 const MainPage = () => {
   return (
     <>
       <HomePage />
       <Blogs />
+      <CopyrightPage />
     </>
   );
 };
