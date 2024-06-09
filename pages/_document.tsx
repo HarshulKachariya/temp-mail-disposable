032d1d5e572023b77a56f219e5cbbfa76b7cdebe
HarshulKachariya <108606007+HarshulKachariya@users.noreply.github.com>
2024-06-09 23:46:39 +0530

    Update _document.tsx and solve crossorigin error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/email.svg" crossorigin="anonymous"/>
+        <link rel="icon" href="/email.svg" crossOrigin="anonymous"/>
         <title>
           TempMail - Get your temporary and disposable email address instantly!
         </title>
