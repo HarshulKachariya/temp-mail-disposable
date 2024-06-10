70b37edce3058d1d9da052543a37b87098115c5e
HarshulKachariya <108606007+HarshulKachariya@users.noreply.github.com>
2024-06-10 20:51:10 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,8 +34,10 @@ export default function Document() {
           name="google-site-verification"
           content="google157ac4885f988124"
         />
-        <meta name="google-adsense-account" content="ca-pub-3938307797483770" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3938307797483770" crossOrigin="anonymous"></script>
+        <meta name="google-adsense-account" content="ca-pub-5728499744349930" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5728499744349930" crossOrigin="anonymous"></script>
+{/*         <meta name="google-adsense-account" content="ca-pub-3938307797483770" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3938307797483770" crossOrigin="anonymous"></script> */}
       </Head>
       <body className="bg-gray-100">
         <Main />
