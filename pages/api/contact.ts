9be01fbb08610dcbb2752c2b5e9f79fc63128ba4
HarshulKachariya <harshulkachariya17@gmail.com>
2024-06-10 22:24:20 +0530

    "Added MongoDB dependency and implemented CRUD operations in API endpoint"

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import clientPromise from "../../lib/mongodb";
 
 type RequestBody = {
   name: string;
@@ -7,22 +8,52 @@ type RequestBody = {
   message: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const handlePostRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { name, email, reason, message } = req.body as RequestBody;
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("tempmail_email");
+    const collection = db.collection("contact");
+
+    const result = await collection.insertOne({
+      name,
+      email,
+      reason,
+      message,
+      createdAt: new Date(),
+    });
+
+    res.status(201).json(result);
+  } catch (error) {
+    console.error("Error handling POST request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const handleGetRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("tempmail_email");
+    const data = await db.collection("contact").find().limit(100).toArray();
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error handling GET request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
-    const { name, email, reason, message } = req.body as RequestBody;
-
-    if (!name || !email || !reason || !message) {
-      return res.status(400).json({ error: "All fields are required." });
-    }
-
-    try {
-      res.status(200).json({ name, email, reason, message });
-    } catch (error) {
-      console.error("Error handling request:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
+    await handlePostRequest(req, res);
+  } else if (req.method === "GET") {
+    await handleGetRequest(req, res);
   } else {
-    res.setHeader("Allow", ["POST"]);
+    res.setHeader("Allow", ["POST", "GET"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
