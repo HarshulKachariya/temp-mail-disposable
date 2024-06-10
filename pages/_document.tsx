6b824a8d518305581ba2d980c766086ee6fe2659
HarshulKachariya <harshulkachariya17@gmail.com>
2024-06-10 22:32:23 +0530

    update document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/email.svg" />
+        <link rel="icon" href="/email.svg" crossOrigin="anonymous" />
+
         <title>
           TempMail - Get your temporary and disposable email address instantly!
         </title>
+
         <meta
           name="description"
           content="TempMail - Get your temporary email address instantly!"
@@ -78,7 +80,14 @@ export default function Document() {
           name="google-site-verification"
           content="google157ac4885f988124"
         />
-        {/* <meta name="google-site-verification" content="WuFxV91DgUW_CNbRf45kqrU3eFpS49q5qqTBnGLhpCM" /> */}
+        <meta name="google-adsense-account" content="ca-pub-5728499744349930" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5728499744349930"
+          crossOrigin="anonymous"
+        ></script>
+        {/*         <meta name="google-adsense-account" content="ca-pub-3938307797483770" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3938307797483770" crossOrigin="anonymous"></script> */}
       </Head>
       <body className="bg-gray-100">
         <Main />
