fb63f3741ed778f0dee79434141803545e31686c
Harshil Sakhiya <harshilsakhiya9886@gmail.com>
2024-07-10 21:05:58 +0530

    build issue fixed

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,30 +1,25 @@
 import React, { useEffect, useRef } from "react";
 
 const Ads: React.FC = () => {
-  const adRef = useRef<HTMLDivElement>(null);
-
   useEffect(() => {
-    if (adRef.current) {
-      try {
-        // Check if the adsbygoogle array exists and push the ad
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-      } catch (e) {
-        console.error('Adsense error:', e);
-      }
+    try {
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
+        {}
+      );
+    }catch (error) {
+      console.log(error);
     }
   }, []);
 
   return (
-    <div ref={adRef}>
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-5728499744349930"
-        data-ad-slot="1183533549"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </div>
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-5728499744349930"
+      data-ad-slot="1183533549"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
   );
 };
 
