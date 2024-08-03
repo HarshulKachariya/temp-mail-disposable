8772e49d848f6bafc07e962a790aa8a17435ddf9
harshil sakhiya <95619537+harshilsakhiya@users.noreply.github.com>
2024-08-03 15:04:44 +0530

    fixed : google analiacs add in document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,15 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
+
 import type { AppProps } from "next/app";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import "@/styles/globals.css";
 const GA_TRACKING_ID = 'G-TLSRBXL8TD';
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url:any) => {
-      window.gtag('config', GA_TRACKING_ID, {
-        page_path: url,
-      });
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+
   return (
     <>
       <Header />
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      
       <Component {...pageProps} />
       <Footer />
     </>
