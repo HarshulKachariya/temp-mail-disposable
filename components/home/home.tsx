6f9564a5af982c61b0ef5ae119d7379fe9a92600
HarshulKachariya <harshulkachariya17@gmail.com>
2024-08-06 23:00:27 +0530

    add the add components in the all compoennts and fix the issue of email fetching if email is exist in localstorage then set that email otherwise fetch new email

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,3 +1,167 @@
+// "use client";
+
+// import { useCallback, useEffect, useRef, useState } from "react";
+
+// import QrCodeIcon from "@mui/icons-material/QrCode";
+// import FileCopyIcon from "@mui/icons-material/FileCopy";
+// import SyncIcon from "@mui/icons-material/Sync";
+// import EditIcon from "@mui/icons-material/Edit";
+// import DeleteIcon from "@mui/icons-material/Delete";
+// import Button from "../button";
+// import QRCode from "react-qr-code";
+// import MailBox from "../mailbox/mailbox";
+
+// import axiosInstance from "@/common/axiosInstance";
+// import CustomButton from "../custombuttons";
+// import Ads from "../ads";
+// import SqureAds from "../squreAds";
+
+// const Home = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [isBtnLoading, setIsBtnLoading] = useState(false);
+//   const [isMailboxLoading, setIsMailboxLoading] = useState(true);
+//   const emailRef = useRef<HTMLInputElement>(null);
+//   const [email, setEmail] = useState<string>("Loading...");
+//   const effectRan = useRef(false);
+
+//   const copyEmailToClipboard = useCallback(() => {
+//     if (emailRef.current) {
+//       emailRef.current.select();
+//       emailRef.current.setSelectionRange(0, 999);
+//       window.navigator.clipboard.writeText(email);
+//     }
+//   }, [email]);
+
+//   const data: any = localStorage.getItem("email");
+//   const emails = JSON.parse(data);
+
+//   const handleFetchEmails = async () => {
+//     try {
+//       const response = await axiosInstance.get("/new");
+//       localStorage.setItem("email", JSON.stringify(response.data));
+//       const { email } = response.data;
+//       setEmail(email);
+//       setIsMailboxLoading(false);
+//       setIsBtnLoading(true);
+//     } catch (error) {
+//       setEmail("Loading...");
+//       setIsBtnLoading(false);
+//       console.log(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (effectRan.current === false) {
+//       if (email === "Loading...") {
+//         if (emails) {
+//           setEmail(data?.email);
+//         } else {
+//           handleFetchEmails();
+//         }
+//         setIsMailboxLoading(false);
+//       }
+//       effectRan.current = true;
+
+//       return () => {
+//         effectRan.current = false;
+//       };
+//     }
+//   }, []);
+
+//   // console.log(JSON.parse(localStorage.getItem("email")));
+
+//   return (
+//     <>
+//       <div className="bg-[#21232a] md:p-7 p-5 flex flex-col  md:grid md:grid-cols-12 gap-5 justify-between items-center w-full md:h-2/3 h-3/4">
+//         {/* <div className="bg-[#21232a] md:p-7 p-5 grid md:grid-cols-12 gap-5 justify-between items-center w-full md:h-2/3 h-3/4"> */}
+//         <div className=" hidden md:grid md:col-span-3">
+//           <SqureAds />
+//         </div>
+//         <div className="md:col-span-6 col-span-12 ">
+//           {/* <div className="max-w-[556px]"> */}
+//           <div className="flex flex-col gap-4 justify-center items-center text-white mx-auto md:p-7 p-5 max-w-[556px] h-full border-2 border-zinc-700 rounded-md border-dashed">
+//             <span className="text-xl font-bold">
+//               Your Temporary Email Address
+//             </span>
+//             <div className="flex w-full md:flex-row flex-col gap-5 md:justify-center relative">
+//               <div className="flex md:flex-row flex-col w-full text-white">
+//                 <input
+//                   type="text"
+//                   readOnly
+//                   value={!isMailboxLoading ? email : "Loading..."}
+//                   ref={emailRef}
+//                   className="p-4 md:w-5/6 w-full h-full text-xl bg-[#2B2C33] rounded-full outline-none"
+//                 />
+//                 <div className="md:flex flex flex-row gap-5 md:absolute md:-right-2 md:-top-4 mt-5 w-full">
+//                   <Button
+//                     name="qrcode"
+//                     onClick={() => setIsOpen(!isOpen)}
+//                     className="bg-zinc-600 gap-1 rounded-full md:w-12 w-full h-12 p-3 items-center justify-center flex md:absolute md:right-24"
+//                   >
+//                     <QrCodeIcon className="text-white text-2xl" />
+//                     <span className="md:hidden">QR Code</span>
+//                   </Button>
+
+//                   <Button
+//                     name="copy"
+//                     onClick={copyEmailToClipboard}
+//                     className="bg-emerald-500 hover:bg-zinc-500 gap-1 rounded-full md:w-12 w-full h-12 p-3 items-center justify-center flex md:absolute md:right-7"
+//                   >
+//                     <FileCopyIcon className="text-white text-2xl" />
+//                     <span className="md:hidden">Copy</span>
+//                   </Button>
+//                 </div>
+//                 <div
+//                   className={`${
+//                     isOpen ? "bg-white p-3 rounded-md h-46 w-46" : "hidden"
+//                   } absolute top-32 left-2 md:top-16 md:left-72`}
+//                 >
+//                   <QRCode
+//                     size={256}
+//                     className={`${isOpen ? "h-40 w-40" : "hidden"}`}
+//                     value={email}
+//                     viewBox={`0 0 256 256`}
+//                   />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <p className=" mt-5 text-zinc-500 text-center">
+//             Forget about spam, advertising mailings, hacking and attacking
+//             robots. Keep your real mailbox clean and secure. Temp Mail provides
+//             temporary, secure, anonymous, free, disposable email address.
+//           </p>
+//         </div>
+//         <div className=" md:grid md:col-span-3">
+//           {/* <div className=" w-[400px] h-[280px] mx-auto hidden md:grid md:col-span-3"> */}
+//           <SqureAds />
+//         </div>
+//       </div>
+//       <div className="flex flex-wrap md:flex-row gap-4 md:gap-7 shadow-2xl  justify-center items-center p-7 bg-white text-black ">
+//         <CustomButton onClick={copyEmailToClipboard} name="copy">
+//           <FileCopyIcon />
+//           Copy
+//         </CustomButton>
+//         <CustomButton disabled={isBtnLoading} onClick={() => {}} name="refresh">
+//           <SyncIcon />
+//           Refresh
+//         </CustomButton>
+//         <CustomButton onClick={handleFetchEmails} name="change">
+//           <EditIcon />
+//           Change
+//         </CustomButton>
+//         <CustomButton onClick={handleFetchEmails} name="delete">
+//           <DeleteIcon />
+//           Delete
+//         </CustomButton>
+//       </div>
+//       <MailBox email={email} />
+//     </>
+//   );
+// };
+
+// export default Home;
+
 "use client";
 
 import { useCallback, useEffect, useRef, useState } from "react";
@@ -35,10 +199,14 @@ const Home = () => {
   const handleFetchEmails = async () => {
     try {
       const response = await axiosInstance.get("/new");
-      const { email } = response.data;
-      setEmail(email);
+      const { email: fetchedEmail, token: any } = response.data;
+      localStorage.setItem(
+        "email",
+        JSON.stringify({ email: fetchedEmail, token: any })
+      );
+      setEmail(fetchedEmail);
       setIsMailboxLoading(false);
-      setIsBtnLoading(true);
+      setIsBtnLoading(false);
     } catch (error) {
       setEmail("Loading...");
       setIsBtnLoading(false);
@@ -48,10 +216,16 @@ const Home = () => {
 
   useEffect(() => {
     if (effectRan.current === false) {
-      if (email === "Loading...") {
-        handleFetchEmails();
+      const storedData = localStorage.getItem("email");
+      const storedEmail = storedData ? JSON.parse(storedData).email : null;
+
+      if (storedEmail) {
+        setEmail(storedEmail);
         setIsMailboxLoading(false);
+      } else {
+        handleFetchEmails();
       }
+
       effectRan.current = true;
 
       return () => {
@@ -63,12 +237,10 @@ const Home = () => {
   return (
     <>
       <div className="bg-[#21232a] md:p-7 p-5 flex flex-col  md:grid md:grid-cols-12 gap-5 justify-between items-center w-full md:h-2/3 h-3/4">
-        {/* <div className="bg-[#21232a] md:p-7 p-5 grid md:grid-cols-12 gap-5 justify-between items-center w-full md:h-2/3 h-3/4"> */}
-        <div className=" hidden md:grid md:col-span-3">
+        <div className="hidden md:grid md:col-span-3">
           <SqureAds />
         </div>
-        <div className="md:col-span-6 col-span-12 ">
-          {/* <div className="max-w-[556px]"> */}
+        <div className="md:col-span-6 col-span-12">
           <div className="flex flex-col gap-4 justify-center items-center text-white mx-auto md:p-7 p-5 max-w-[556px] h-full border-2 border-zinc-700 rounded-md border-dashed">
             <span className="text-xl font-bold">
               Your Temporary Email Address
@@ -91,7 +263,6 @@ const Home = () => {
                     <QrCodeIcon className="text-white text-2xl" />
                     <span className="md:hidden">QR Code</span>
                   </Button>
-
                   <Button
                     name="copy"
                     onClick={copyEmailToClipboard}
@@ -122,8 +293,7 @@ const Home = () => {
             temporary, secure, anonymous, free, disposable email address.
           </p>
         </div>
-        <div className=" md:grid md:col-span-3">
-          {/* <div className=" w-[400px] h-[280px] mx-auto hidden md:grid md:col-span-3"> */}
+        <div className="md:grid md:col-span-3">
           <SqureAds />
         </div>
       </div>
@@ -132,7 +302,11 @@ const Home = () => {
           <FileCopyIcon />
           Copy
         </CustomButton>
-        <CustomButton disabled={isBtnLoading} onClick={() => {}} name="refresh">
+        <CustomButton
+          disabled={isBtnLoading}
+          onClick={handleFetchEmails}
+          name="refresh"
+        >
           <SyncIcon />
           Refresh
         </CustomButton>
