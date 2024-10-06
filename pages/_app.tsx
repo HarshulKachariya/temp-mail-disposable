296a4cdfb7a5e5782b6abdbd940703d02fb28afc
HarshulKachariya <harshulkachariya17@gmail.com>
2024-10-06 20:37:26 +0530

    "Added global stylesheet import to _app.tsx and uncommented useEffect block in [slug].tsx"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 const Header = dynamic(() => import("@/components/header"), {
   loading: () => <></>,
